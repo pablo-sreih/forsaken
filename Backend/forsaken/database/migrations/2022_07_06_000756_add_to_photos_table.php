7365d125d1371f732e94f7fba168ef1f185720f7
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-07-06 12:13:24 +0300

    Added Albums Table

---
+++ Backend/forsaken/database/migrations/2022_07_06_000756_add_to_photos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('photos', function (Blueprint $table) {
+            $table->integer('location_id');
+            $table->integer('user_id');
+            $table->text('caption');
+            $table->integer('album_id');
+            $table->longText('image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('photos', function (Blueprint $table) {
+            //
+        });
+    }
+};
