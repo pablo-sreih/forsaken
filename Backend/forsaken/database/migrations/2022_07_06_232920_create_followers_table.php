43377c3a17718a5c238db964893962956346001d
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-07-07 02:36:48 +0300

    Added Columns to Followers Table

--- Backend/forsaken/database/migrations/2022_07_06_232920_create_followers_table.php
+++ Backend/forsaken/database/migrations/2022_07_06_232920_create_followers_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
         Schema::create('followers', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->integer('user_id');
+            $table->integer('follower_id');
         });
     }
 
