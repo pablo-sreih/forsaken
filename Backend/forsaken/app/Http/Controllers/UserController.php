299da6314e30cb7ce1a4715d2c1bb96ed6a35121
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-07-07 16:53:06 +0300

    Added Add About and Get User APIS

--- Backend/forsaken/app/Http/Controllers/UserController.php
+++ Backend/forsaken/app/Http/Controllers/UserController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    public function addAbout(Request $request) {
+        $about = $request->about;
+        $user_id = $request->user_id;
+        $user = User::find($user_id);
+        $user -> about = $about;
+        $user->save();
+
+        return response()->json([
+            'status' => "success",
+            'user' => $user,
+        ],200);
+    }
+
+    public function getUser(Request $request) {
+        $user_id = $request->user_id;
+        $user = User::find($user_id);
+
+        return response()->json([
+            'status' => 'success',
+            'user' => $user,
+        ],200);
+    }
+
 }
--- Backend/forsaken/routes/api.php
+++ Backend/forsaken/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TodoController;
+use App\Http\Controllers\UserController;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
@@ -27,3 +28,6 @@ Route::controller(AuthController::class)->group(function () {
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/addAbout', [UserController::class, 'addAbout']);
+Route::post('/getUser', [UserController::class, 'getUser']);
