e88c7a65bf0a9e3f708db8f7a32f5ea1c4c77984
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-07-07 03:11:29 +0300

    Added Total Followers and Following Tables

--- Backend/forsaken/database/migrations/2022_07_07_000839_add_to_users_table.php
+++ Backend/forsaken/database/migrations/2022_07_07_000839_add_to_users_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->integer('total_followers');
+            $table->integer('total_following');
         });
     }
 
