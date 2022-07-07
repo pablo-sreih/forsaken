8df57767f8d518a8650aa21238972890ccc73eec
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-07-07 03:29:33 +0300

    Added Columns to Photo Comments Table

--- Backend/forsaken/database/migrations/2022_07_06_225426_create_photo_comments_table.php
+++ Backend/forsaken/database/migrations/2022_07_06_225426_create_photo_comments_table.php
@@ -16,6 +16,9 @@ return new class extends Migration
         Schema::create('photo_comments', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->integer('user_id');
+            $table->integer('photo_id');
+            $table->text('comment');
         });
     }
 
