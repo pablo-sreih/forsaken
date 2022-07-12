7a9d580f80b4261ef837f469646661afb8b2bab3
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-07-13 02:54:26 +0300

    Backend: Dropped Name Column in Locations Table

--- Backend/forsaken/database/migrations/2022_07_12_235204_add_to_locations_table.php
+++ Backend/forsaken/database/migrations/2022_07_12_235204_add_to_locations_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('locations', function (Blueprint $table) {
-            //
+            $table->dropColumn('name');
         });
     }
 
