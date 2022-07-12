646f5b85a8a62a563de608117d65a21d0ab29b0b
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-07-13 02:25:57 +0300

    Backend: Added Get All Locations API

--- Backend/forsaken/app/Http/Controllers/LocationController.php
+++ Backend/forsaken/app/Http/Controllers/LocationController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Location;
 
 class LocationController extends Controller
 {
-    //
+    public function getAllLocations(Request $request) {
+        $locations = Location::all();
+
+        return response()->json([
+            'status' => 'success',
+            'locations' => $locations,
+        ],200);
+    } 
 }
