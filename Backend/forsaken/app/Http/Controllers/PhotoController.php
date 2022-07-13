13085b8e1b1764e8292deaec688818d2c838596e
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-07-13 03:31:33 +0300

    Backend: Added Add Photo API

--- Backend/forsaken/app/Http/Controllers/PhotoController.php
+++ Backend/forsaken/app/Http/Controllers/PhotoController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Photo;
 
 class PhotoController extends Controller
 {
-    //
+    public function addPhoto(Request $request) {
+        $photo = new Photo();
+        $photo->location_id = $request->location_id;
+        $photo->user_id = $request->user_id;
+        $photo->caption = $request->caption;
+        $photo->image = $request->image;
+        $photo->total_likes = 0;
+        $photo->save();
+
+        return response()->json([
+            'status' => 'success',
+            'photo' => $photo,
+        ],200);
+    }
 }
