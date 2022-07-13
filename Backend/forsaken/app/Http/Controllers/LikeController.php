4c1818525a53c83e5017e065477272a56c21153e
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-07-13 18:43:23 +0300

    Backend: Added Like API

--- Backend/forsaken/app/Http/Controllers/LikeController.php
+++ Backend/forsaken/app/Http/Controllers/LikeController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class LikeController extends Controller
 {
-    //
+    public function addLike(Request $request) {
+        $like = new Like();
+        $user_id = auth()->user()->id;
+        $like->user_id = $user_id;
+        $like->post_id = $request->post_id;
+        $like->save();
+
+        return response()->json([
+            'status' => 'success',
+            'like' => $like,
+        ],200);
+    }
 }
