f0974e930054c906771c4a6ac80e67e27858b717
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-07-13 19:01:35 +0300

    Backend: Added Add Activity API

--- Backend/forsaken/app/Http/Controllers/ActivityController.php
+++ Backend/forsaken/app/Http/Controllers/ActivityController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Activity;
 
 class ActivityController extends Controller
 {
-    //
+    public function addActivity(Request $request) {
+        $activity = new Activity();
+        $user_id = auth()->user()->id;
+        $activity->user_id = $user_id;
+        $activity->location_id = $request->location_id;
+        $activity->user_emf_reading = $request->user_emf_reading;
+        $activity->save();
+
+        return response()->json([
+            'status' => 'success',
+            'activity' => $activity,
+        ],200);
+    }
 }
