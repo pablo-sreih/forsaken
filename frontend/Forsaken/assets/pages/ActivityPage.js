c098fc5e855d67a8db7a1ac3be514fa68cbfe109
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-07-17 14:09:45 +0300

    Frontend: Added Styling to Activity Page

--- frontend/Forsaken/assets/pages/ActivityPage.js
+++ frontend/Forsaken/assets/pages/ActivityPage.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity,Image, View } from 'react-native';
 import { Magnetometer } from 'expo-sensors';
 import { useFonts } from 'expo-font';
 import Header from '../components/Header';
+
+const image = require ( '../logos/logo_emf.png' )
 
 export default function ActivityPage() {
 
@@ -44,7 +46,9 @@ export default function ActivityPage() {
   const [loaded] = useFonts({
     montserratBlack : require('../fonts/Montserrat-Black.ttf'),
     montserratExtraBold: require('../fonts/Montserrat-ExtraBold.ttf'),
-    montserratBold: require('../fonts/Montserrat-Bold.ttf')
+    montserratBold: require('../fonts/Montserrat-Bold.ttf'),
+    montserratSemiBold: require ('../fonts/Montserrat-SemiBold.ttf'),
+    montserratRegular: require ('../fonts/Montserrat-Regular.ttf')
   })
 
   if (!loaded) {
@@ -54,25 +58,10 @@ export default function ActivityPage() {
   return (
     <View style={styles.container}>
     <Header name='EMF TRACKER'/>
-      <Text style={styles.text}>Magnetometer:</Text>
-      <Text style={{fontSize: 20}}>
-        x: {round(x)} y: {round(y)} z: {round(z)}
+    <Image style={styles.image} source={image}/>
+      <Text style={styles.emfTracker}>
+        {Math.sqrt(x**2 + y**2 + z**2).toFixed(2)} μT
       </Text>
-      <Text>μT formula: Math.sqrt(x**2 + y**2 + z**2).toFixed(2)</Text>
-      <Text style={{fontFamily: 'montserratBold',fontSize: 40}}>
-        microtesla: {Math.sqrt(x**2 + y**2 + z**2).toFixed(2)} μT
-      </Text>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} style={styles.button}>
-          <Text>{subscription ? 'On' : 'Off'}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={_slow} style={[styles.button, styles.middleButton]}>
-          <Text>Slow</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={_fast} style={styles.button}>
-          <Text>Fast</Text>
-        </TouchableOpacity>
-      </View>
     </View>
   );
 }
@@ -80,7 +69,22 @@ export default function ActivityPage() {
 const styles = StyleSheet.create({
     container: {
         flex:1,
+    },
+
+    image:{
+        marginTop: 40,
+        width: 125,
+        height: 125,
+        alignSelf: 'center'
+    },
+
+    emfTracker: {
+        marginTop: 40,
+        fontFamily: 'montserratRegular',
+        fontSize: 40,
+        alignSelf: 'center'
     }
+
 })
 
 const round = Math.floor
--- frontend/Forsaken/assets/pages/FeedPage.js
+++ frontend/Forsaken/assets/pages/FeedPage.js
@@ -10,7 +10,9 @@ export default function FeedPage() {
   const [loaded] = useFonts({
     montserratBlack : require('../fonts/Montserrat-Black.ttf'),
     montserratExtraBold: require('../fonts/Montserrat-ExtraBold.ttf'),
-    montserratBold: require('../fonts/Montserrat-Bold.ttf')
+    montserratBold: require('../fonts/Montserrat-Bold.ttf'),
+    montserratSemiBold: require ('../fonts/Montserrat-SemiBold.ttf'),
+    montserratRegular: require ('../fonts/Montserrat-Regular.ttf')
   })
 
   if (!loaded) {
