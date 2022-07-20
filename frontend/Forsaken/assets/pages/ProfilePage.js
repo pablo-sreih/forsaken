f09ca55bb69f425733402b64de38e2cc44de2a20
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-07-20 15:54:47 +0300

    Frontend: Added Profile Name

--- frontend/Forsaken/assets/pages/ProfilePage.js
+++ frontend/Forsaken/assets/pages/ProfilePage.js
@@ -1,13 +1,27 @@
-import { View, StyleSheet } from "react-native"
+import { View, StyleSheet, Text } from "react-native"
 import Header from "../components/Header"
+import { useFonts } from "expo-font"
+
 
 export default function ProfilePage() {
+
+    const [loaded] = useFonts({
+        montserratBlack : require('../fonts/Montserrat-Black.ttf'),
+        montserratExtraBold: require('../fonts/Montserrat-ExtraBold.ttf'),
+        montserratBold: require('../fonts/Montserrat-Bold.ttf'),
+        montserratSemiBold: require ('../fonts/Montserrat-SemiBold.ttf'),
+        montserratRegular: require ('../fonts/Montserrat-Regular.ttf')
+    })
+    
+    if (!loaded) {
+        return null;
+    }
+
     return(
         <View style={styles.container}>
             <Header name="PROFILE"/>
-            <View style={styles.profilePicContainer}>
-
-            </View>
+            <View style={styles.profilePicContainer}></View>
+            <Text style={styles.profileName}>John Doe</Text>
         </View>
     )
 }
@@ -24,5 +38,12 @@ const styles = StyleSheet.create({
         height: 150,
         backgroundColor: 'gray',
         borderRadius: 100
+    },
+
+    profileName: {
+        fontFamily: 'montserratBold',
+        fontSize: 20,
+        alignSelf: 'center',
+        marginTop: 10
     }
 })
