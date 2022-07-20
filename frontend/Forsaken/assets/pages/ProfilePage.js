e9e69e543fba140b96d44cb05640179b9288562e
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-07-20 15:41:45 +0300

    Frontend: Added Profile Picture Placeholder

--- frontend/Forsaken/assets/pages/ProfilePage.js
+++ frontend/Forsaken/assets/pages/ProfilePage.js
@@ -5,6 +5,9 @@ export default function ProfilePage() {
     return(
         <View style={styles.container}>
             <Header name="PROFILE"/>
+            <View style={styles.profilePicContainer}>
+
+            </View>
         </View>
     )
 }
@@ -12,5 +15,14 @@ export default function ProfilePage() {
 const styles = StyleSheet.create({
     container: {
         flex: 1
+    },
+
+    profilePicContainer: {
+        marginTop: 20,
+        alignSelf: 'center',
+        width: 150,
+        height: 150,
+        backgroundColor: 'gray',
+        borderRadius: 100
     }
 })
