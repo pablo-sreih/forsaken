5600e448accdd84d63eb063f412f719f4bf788e9
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-07-23 07:14:04 +0300

    Frontend: Updated Login Screen

--- frontend/Forsaken/assets/pages/LoginPage.js
+++ frontend/Forsaken/assets/pages/LoginPage.js
@@ -1,10 +1,13 @@
-import { View, Text } from "react-native"
+import { View, Text, TouchableOpacity } from "react-native"
 
 
 export default function LoginPage(){
     return(
         <View>
             <Text>Login Page</Text>
+            <TouchableOpacity>
+                <Text>Register</Text>
+            </TouchableOpacity>
         </View>
     )
 }
