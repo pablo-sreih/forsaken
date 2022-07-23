53d0ab469e9239d22f10dab5ace865bd66adeb0e
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-07-23 09:43:32 +0300

    Frontend: Added Styling to Sign In Page

--- frontend/Forsaken/assets/pages/SigninPage.js
+++ frontend/Forsaken/assets/pages/SigninPage.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native"
+import { View, Text, TouchableOpacity, StyleSheet, TextInput } from "react-native"
 import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
 
@@ -23,6 +23,11 @@ export default function SigninPage({ navigation }){
     return(
         <View style={styles.container}>
             <Text style={styles.title}>Sign In</Text>
+            <TextInput style={styles.inputField} placeholder='Email'></TextInput>
+            <TextInput style={styles.inputField} placeholder='Password'></TextInput>
+            <TouchableOpacity style={styles.signinButton}>
+                <Text style={styles.buttonText}>Sign In</Text>
+            </TouchableOpacity>
             <TouchableOpacity onPress={() => navigation.navigate('Register')}>
                 <Text>Register</Text>
             </TouchableOpacity>
@@ -34,12 +39,41 @@ export default function SigninPage({ navigation }){
 const styles = StyleSheet.create({
     container: {
         marginTop: 50,
-        flex: 1
+        flex: 1,
+        justifyContent: 'center'
+    },
+
+    inputField: {
+        borderWidth: 2,
+        width: '80%',
+        alignSelf: 'center',
+        borderRadius: 5,
+        fontSize: 15,
+        padding: 7,
+        fontFamily: 'montserratSemiBold',
+        marginTop: 20
     },
 
     title: {
         fontSize: 30,
         fontFamily: 'montserratBold',
-        alignSelf: 'center'
+        alignSelf: 'center',
+        marginBottom: 50
+    },
+
+    signinButton: {
+        borderWidth: 2,
+        width: '80%',
+        alignSelf: 'center',
+        padding: 10,
+        alignItems: 'center',
+        marginTop: 20,
+        borderRadius: 5,
+        backgroundColor: 'black'
+    },
+
+    buttonText: {
+        fontFamily: 'montserratBold',
+        color: 'white'
     }
 })
