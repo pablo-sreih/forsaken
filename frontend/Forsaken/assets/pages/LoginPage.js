ecaccc17c9e249751378e586d00c9318e01c697b
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-07-23 09:18:21 +0300

    Frontend: Added Styling to Sign In Page

--- frontend/Forsaken/assets/pages/LoginPage.js
+++ frontend/Forsaken/assets/pages/LoginPage.js
@@ -1,15 +1,32 @@
 import React from "react"
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native"
+import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
 
 
 
 export default function LoginPage({ navigation }){
+
+    const [loaded] = useFonts({
+        montserratBlack : require('../fonts/Montserrat-Black.ttf'),
+        montserratExtraBold: require('../fonts/Montserrat-ExtraBold.ttf'),
+        montserratBold: require('../fonts/Montserrat-Bold.ttf'),
+        montserratSemiBold: require ('../fonts/Montserrat-SemiBold.ttf'),
+        montserratRegular: require ('../fonts/Montserrat-Regular.ttf')
+    })
+    
+    if (!loaded) {
+        return null;
+    }
+
+
     return(
         <View style={styles.container}>
-            <Text>Login Page</Text>
+            <Text style={styles.title}>Sign In</Text>
             <TouchableOpacity onPress={() => navigation.navigate('Register')}>
                 <Text>Register</Text>
             </TouchableOpacity>
+            <StatusBar style='auto'/>
         </View>
     )
 }
@@ -18,5 +35,11 @@ const styles = StyleSheet.create({
     container: {
         marginTop: 50,
         flex: 1
+    },
+
+    title: {
+        fontSize: 30,
+        fontFamily: 'montserratBold',
+        alignSelf: 'center'
     }
 })
