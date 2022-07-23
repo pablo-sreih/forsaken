7d6048c45b53e77bd5c85b158158ec69857c2410
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-07-23 23:25:05 +0300

    Frontend: Added Styling to Image in Photo Container

--- frontend/Forsaken/assets/components/MiniPhoto.js
+++ frontend/Forsaken/assets/components/MiniPhoto.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { View, Image, StyleSheet } from "react-native";
+import { View, Image, StyleSheet, Text } from "react-native";
+
+const image = require('../images/back.jpg')
 
 export default function MiniPhoto() {
     return(
         <View style={styles.container}>
-            
+            <Image style={styles.image} source={image}/>
         </View>
     )
 }
@@ -16,5 +18,12 @@ const styles = StyleSheet.create({
         height: '53%',
         borderRadius: 10,
         margin:3
+    },
+
+    image: {
+        flex: 1,
+        width: '100%',
+        resizeMode: 'cover',
+        borderRadius: 10
     }
 })
