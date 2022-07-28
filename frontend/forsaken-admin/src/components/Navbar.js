5610fae93a279e15e4cab0ae03015fcba7ccf782
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-07-28 10:42:29 +0300

    Frontend: Added Styling to Navbar

--- frontend/forsaken-admin/src/components/Navbar.js
+++ frontend/forsaken-admin/src/components/Navbar.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import { SidebarData } from "./SidebarData";
+import { IconContext } from "react-icons";
 
 export default function Navbar() {
   const [sidebar, setSidebar] = useState(false);
@@ -10,30 +11,32 @@ export default function Navbar() {
 
   return (
     <>
-      <div className="navbar">
-        <Link to="#" className="menu-bars">
-          <FaIcons.FaBars />
-        </Link>
-      </div>
-      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-        <ul className="nav-menu-items">
-          <li className="navbar-toggle">
-            <Link to="#" className="menu-bars">
-              <AiIcons.AiOutlineClose />
-            </Link>
-          </li>
-          {SidebarData.map((item, index) => {
-            return (
-              <li key={index} className={item.cName}>
-                <Link to={item.path}>
-                  {item.icon}
-                  <span>{item.title}</span>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <div className="navbar">
+          {/* <Link to="#" className="menu-bars">
+            <FaIcons.FaBars onClick={showSidebar} />
+          </Link> */}
+        </div>
+        <nav className={"nav-menu active"}>
+          <ul className="nav-menu-items">
+            {/* <li className="navbar-toggle">
+              <Link to="#" className="menu-bars">
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li> */}
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
     </>
   );
 }
