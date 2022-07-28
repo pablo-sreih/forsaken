6351a34cafe294f3c41f9ecdda7184c5fb32a5d1
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-07-28 14:25:07 +0300

    Frontend: Added Locations Page to Admin Panel

--- frontend/forsaken-admin/src/pages/Locations.js
+++ frontend/forsaken-admin/src/pages/Locations.js
@@ -3,7 +3,40 @@ import React from "react";
 function Locations() {
   return (
     <div className="locations">
-      <h1>Locations</h1>
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>Location Name</th>
+          <th>Latitude</th>
+          <th>Longitude</th>
+          <th>Average Rating</th>
+          <th>Average EMF</th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td></td>
+          <td>Pablo Sreih</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td></td>
+          <td>Vladimir Mawla</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td></td>
+          <td>Alaa Al Tawil</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td></td>
+          <td>Charbel Daoud</td>
+          <td></td>
+        </tr>
+      </table>
     </div>
   );
 }
