9e00824327ed4e633360cfdf56266a31b5dbd40f
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-07-28 11:24:22 +0300

    Frontend: Added Welcome Message

--- frontend/forsaken-admin/src/components/Navbar.js
+++ frontend/forsaken-admin/src/components/Navbar.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 import { SidebarData } from "./SidebarData";
 import { IconContext } from "react-icons";
-
 export default function Navbar() {
   return (
     <>
       <IconContext.Provider value={{ color: "#fff" }}>
         <nav className={"nav-menu"}>
+          <h3 className="welcome">Welcome Admin</h3>
           <ul className="nav-menu-items">
             {SidebarData.map((item, index) => {
               return (
