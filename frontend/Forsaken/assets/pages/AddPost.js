d736ddf1b159bc144a57786e1b7e87fb9beaf32b
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-07-28 16:29:56 +0300

    Frontend: Added Styling to Add Post Page

--- frontend/Forsaken/assets/pages/AddPost.js
+++ frontend/Forsaken/assets/pages/AddPost.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet, TouchableOpacity, Text } from "react-native";
 import Header from "../components/Header";
 import { useFonts } from "expo-font";
+import { TextInput } from "react-native";
 
 export default function AddPost() {
   const [loaded] = useFonts({
@@ -19,7 +20,20 @@ export default function AddPost() {
   return (
     <View style={styles.container}>
       <Header name="ADD POST" />
-      <Text>Hello</Text>
+      <View style={styles.div}>
+        <Text style={styles.label}>Choose Image:</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.label1}>Choose From Gallery</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <Text style={styles.label}>Add Caption</Text>
+        <TextInput style={styles.input}></TextInput>
+      </View>
+      <View>
+        <Text style={styles.label}>Choose Location</Text>
+        <TextInput style={styles.input}></TextInput>
+      </View>
     </View>
   );
 }
@@ -27,5 +41,42 @@ export default function AddPost() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "white",
+  },
+  label: {
+    fontFamily: "montserratBold",
+    margin: 20,
+  },
+  label1: {
+    fontFamily: "montserratBold",
+    // color: "white",
+  },
+  div: {
+    flexDirection: "row",
+    alignContent: "center",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  button: {
+    // backgroundColor: "#24A0ED",
+    borderWidth: 1,
+    height: 30,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+    width: 180,
+    borderColor: "#24A0ED",
+    marginRight: 20,
+  },
+
+  input: {
+    height: 100,
+    borderWidth: 1,
+    borderColor: "#24A0ED",
+    marginLeft: 20,
+    marginRight: 20,
+    borderRadius: 10,
+    textAlignVertical: "top",
+    padding: 10,
   },
 });
