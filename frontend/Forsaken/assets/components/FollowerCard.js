a171534b917933e6ab95b459ca37a051f50f5b30
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-08-02 16:17:58 +0300

    Frontend: Added Styling to Follower Card

--- frontend/Forsaken/assets/components/FollowerCard.js
+++ frontend/Forsaken/assets/components/FollowerCard.js
@@ -1,6 +1,49 @@
 import React from "react";
-import { View } from "react-native-web";
+import { View, StyleSheet, Image, Text } from "react-native";
 
-export default function FollowerCard() {
-  return <View></View>;
+export default function FollowerCard(props) {
+  return (
+    <View>
+      <View style={styles.profileContainer}>
+        <View style={styles.profile}>
+          <Image
+            source={{ uri: props.prof }}
+            style={{ flex: 1, resizeMode: "cover", borderRadius: 100 }}
+          />
+        </View>
+        <View style={styles.profileInfo}>
+          <Text style={styles.name}>{props.name}</Text>
+        </View>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  profileContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignContent: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    marginLeft: 20,
+    marginTop: 20,
+  },
+
+  profile: {
+    width: 55,
+    height: 55,
+    backgroundColor: "gray",
+    borderRadius: 50,
+  },
+
+  name: {
+    fontFamily: "montserratSemiBold",
+    fontSize: 15,
+  },
+  profileInfo: {
+    flex: 1,
+    marginLeft: 14,
+    justifyContent: "center",
+  },
+});
--- frontend/Forsaken/assets/pages/FollowersPage.js
+++ frontend/Forsaken/assets/pages/FollowersPage.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 import Header from "../components/Header";
+import FollowerCard from "../components/FollowerCard";
 
 export default function FollowersPage() {
   return (
     <View style={styles.container}>
       <Header name="FOLLOWERS" />
+      <ScrollView>
+        <FollowerCard />
+      </ScrollView>
     </View>
   );
 }
