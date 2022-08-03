a2c11217750ba4ee64f26abca971f1b2dc8acdfc
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-08-04 00:20:03 +0300

    Frontend: Added Linking to Get Counts API

--- frontend/forsaken-admin/src/pages/Home.js
+++ frontend/forsaken-admin/src/pages/Home.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../components/Card";
+import axios from "axios";
+import { useEffect } from "react";
 
 function Home(props) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const array = [];
+    axios
+      .get(`http://127.0.0.1:8000/api/getCounts`)
+      .then((response) => {
+        array.push(response.data);
+      })
+      .then(() => {
+        console.log(data);
+        setData(array);
+      });
+  }, []);
+
   return (
     <div className="home">
       <h1>Statistics</h1>
       <div style={{ display: "flex", flexDirection: "row", gap: "50px" }}>
-        <Card title="Total Users" info="7" />
-        <Card title="Total Posts" info="18" />
+        <Card title="Total Users" info={data[0].users} />
+        <Card title="Total Posts" info={data[0].posts} />
       </div>
       <div style={{ display: "flex", flexDirection: "row", gap: "50px" }}>
-        <Card title="Total Locations" info="10" />
-        <Card title="Highest EMF" info="230" />
+        <Card title="Total Locations" info={data[0].locations} />
+        <Card title="Highest EMF" info={data[0].high_emf} />
       </div>
     </div>
   );
