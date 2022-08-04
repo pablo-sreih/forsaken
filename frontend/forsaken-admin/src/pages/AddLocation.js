8209a2cb406cb9e58d343da9392e652d400b5d90
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-08-04 10:21:39 +0300

    Frontend: Added UseRef to Inputs

--- frontend/forsaken-admin/src/pages/AddLocation.js
+++ frontend/forsaken-admin/src/pages/AddLocation.js
@@ -2,32 +2,39 @@ import React from "react";
 import { useRef } from "react";
 
 function AddLocation() {
+  const name = useRef(null);
+  const city = useRef(null);
+  const description = useRef(null);
+  const latitude = useRef(null);
+  const longitude = useRef(null);
+  const avg_emf = useRef(null);
+
   return (
     <div className="add-location">
       <h1 style={{ marginBottom: 50 }}>Add Location</h1>
       <div className="input-location">
         <label className="label-location">Location Name</label>
-        <input className="location-input"></input>
+        <input ref={name} className="location-input"></input>
       </div>
       <div className="input-location">
         <label className="label-location">Location City</label>
-        <input className="location-input"></input>
+        <input ref={city} className="location-input"></input>
       </div>
       <div className="input-location">
         <label className="label-location">Location Description</label>
-        <textarea className="location-input"></textarea>
+        <textarea ref={description} className="location-input"></textarea>
       </div>
       <div className="input-location">
         <label className="label-location">Latitude</label>
-        <input className="location-input"></input>
+        <input ref={latitude} className="location-input"></input>
       </div>
       <div className="input-location">
         <label className="label-location">Longitude</label>
-        <input className="location-input"></input>
+        <input ref={longitude} className="location-input"></input>
       </div>
       <div className="input-location">
         <label className="label-location">Average EMF</label>
-        <input className="location-input"></input>
+        <input ref={avg_emf} className="location-input"></input>
       </div>
 
       <button className="location-button" type="submit">
